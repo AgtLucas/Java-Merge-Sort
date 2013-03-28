67dd8e7a5418befcb04972154e07c7536ddf5dcc
Lucas <metallucas.ds@gmail.com>
2013-03-28 18:36:01 -0300

    Metodos merge e intercala

--- src/aula04/mergesort/Aula04MergeSort.java
+++ src/aula04/mergesort/Aula04MergeSort.java
@@ -13,7 +13,22 @@ public class Aula04MergeSort {
     /**
      * @param args the command line arguments
      */
+    
+    public static void merge(String x[], int inicio, int fim) {
+        int meio;
+        if (inicio < fim) {
+            meio = (inicio + fim) / 2;
+            merge(x, inicio, meio);
+            merge(x, meio + 1, fim);
+            intercala(x, inicio, fim, meio);
+        }
+    }
+    
+    public static void intercala(String x[], int inicio, int fim, int meio) {
+        
+    }
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
 }
